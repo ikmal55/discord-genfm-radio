d88e5407e63e8296fad31dfcf3bac88c9e300bb6
ikmal55 <ikmalimani55@gmail.com>
2026-01-28 12:55:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const { 
-  Client, 
-  GatewayIntentBits 
+const {
+  Client,
+  GatewayIntentBits
 } = require("discord.js");
 
 const {
   joinVoiceChannel,
   createAudioPlayer,
   createAudioResource,
-  AudioPlayerStatus,
+  AudioPlayerStatus
 } = require("@discordjs/voice");
+
+const prism = require("prism-media");
+
+// ================= CONFIG =================
+const TOKEN = process.env.TOKEN;
+const RADIO_URL = "http://103.246.184.62:1935/noice_genfm/genfm/playlist.m3u8";
+// ==========================================
 
 const client = new Client({
   intents: [
@@ -19,8 +26,6 @@ const client = new Client({
   ],
 });
 
-const RADIO_URL = "http://103.246.184.62:1935/noice_genfm/genfm/playlist.m3u8";
-
 client.once("ready", () => {
   console.log(`BOT READY: ${client.user.tag}`);
 });
@@ -28,30 +33,45 @@ client.once("ready", () => {
 client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
 
+  // TEST BOT HIDUP
+  if (message.content === "!ping") {
+    return message.reply("pong 🏓");
+  }
+
+  // PLAY RADIO
   if (message.content === "!radio") {
     if (!message.member.voice.channel) {
-      return message.reply("Masuk voice dulu");
+      return message.reply("Masuk voice dulu ya 🎧");
     }
 
     const connection = joinVoiceChannel({
       channelId: message.member.voice.channel.id,
       guildId: message.guild.id,
       adapterCreator: message.guild.voiceAdapterCreator,
+      selfDeaf: false
     });
 
     const player = createAudioPlayer();
 
-    const resource = createAudioResource(RADIO_URL, {
-      inlineVolume: true
+    const ffmpeg = new prism.FFmpeg({
+      args: [
+        "-re",
+        "-i", RADIO_URL,
+        "-analyzeduration", "0",
+        "-loglevel", "0",
+        "-f", "s16le",
+        "-ar", "48000",
+        "-ac", "2"
+      ]
     });
 
-    resource.volume.setVolume(1);
+    const resource = createAudioResource(ffmpeg);
 
     player.play(resource);
     connection.subscribe(player);
 
-    message.reply("📻 Oink Radio GenFM diputar!");
+    message.reply("📻 Oink Radio GenFM ON AIR!");
   }
 });
 
-client.login(process.env.TOKEN);
+client.login(TOKEN);
