0eaf28f5eece28eca8baaba61b0b9b79459738fc
ikmal55 <ikmalimani55@gmail.com>
2026-01-28 12:38:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -67,3 +67,14 @@ client.on("messageCreate", async (message) => {
     message.reply("Bot masuk voice ✅");
   }
 });
+
+const { Client, Intents } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.MESSAGE_CONTENT
+  ]
+});
